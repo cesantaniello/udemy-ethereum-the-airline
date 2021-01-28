780abd8c9a3b023fb2b433b1239711779aa422de
Carlos <carlos.e.santaniello@gmail.com>
2021-01-28 21:53:53 +0100

    6. Tests 2

--- test/Airline.test.js
+++ test/Airline.test.js
@@ -11,4 +11,29 @@ contract('Airline', accounts => {
         let total = await instance.totalFlights();
         assert(total > 0);
     });
+
+    it('should allow customers to buy a flight providing its value', async () => {
+
+        let flight = await instance.flights(0);
+        let flightName = flight[0], price = flight[1];
+
+        await instance.buyFlight(0, { from: accounts[0], value: price });
+        let customerFlight = await instance.customerFlights(accounts[0], 0);
+        let customerTotalFlights = await instance.customerTotalFlights(accounts[0]);
+
+        assert(customerFlight[0], flightName);
+        assert(customerFlight[1], price);
+        assert(customerTotalFlights, 1);
+    });
+
+    it('should not allow customers to buy flights under the price', async () => {
+
+        let flight = await instance.flights(0);
+        let price = flight[1] - 5000;
+        try {
+            await instance.buyFlight(0, { from: accounts[0], value: price});
+        }
+        catch (e) { return; }
+        assert.fail();
+    });
 });
