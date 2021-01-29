df426268e1c7f59f6d1011b1077c59ef070f22d8
Carlos <carlos.e.santaniello@gmail.com>
2021-01-29 20:36:42 +0100

    11. Create App.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,30 @@
 import React, { Component } from "react";
 import Panel from "./Panel";
+import getWeb3 from "./getWeb3";
 
 export class App extends Component {
 
     constructor(props) {
         super(props);
+        this.safe = {
+            account: undefined
+        };
     }
+
+    async componentDidMount(){
+        this.web3 = await getWeb3();
+
+        var account = (await this.web3.eth.getAccounts())[0];
+        
+        this.setState({
+            account: account.toLowerCase()
+        }, () => {
+            this.load();
+        });
+    }
+
+    async load() {}
+
 
     render() {
         return <React.Fragment>
