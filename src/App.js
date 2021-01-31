8f7c56c16e0a0ade11f3a6af813e60ed44b96e3e
Carlos <carlos.e.santaniello@gmail.com>
2021-01-31 18:26:32 +0100

    16. Create interactive service with smart contract

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from "react";
 import Panel from "./Panel";
 import getWeb3 from "./getWeb3";
 import AirlineContract from "./airline";
+import { AirlineService } from "./airlineService";
 
 const converter = (web3) => {
     return(value) => {
@@ -23,7 +24,8 @@ export class App extends Component {
         this.web3 = await getWeb3();
         this.toEther = converter(this.web3);
         this.airline = await AirlineContract(this.web3.currentProvider);
-
+        this.airlineService = new AirlineService(this.airline);
+        
         var account = (await this.web3.eth.getAccounts())[0];
         
         this.setState({
